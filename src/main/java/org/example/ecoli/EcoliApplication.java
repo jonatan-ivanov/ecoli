574f9064d792b06c92f9ee0028012a5b15c623a0
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2024-10-24 16:57:25 -0700

    Add OTLP support with Brave

--- src/main/java/org/example/ecoli/EcoliApplication.java
+++ src/main/java/org/example/ecoli/EcoliApplication.java
@@ -1,11 +1,21 @@
 package org.example.ecoli;
 
+import brave.handler.MutableSpan;
+import zipkin2.reporter.BytesEncoder;
+import zipkin2.reporter.otel.brave.OtlpProtoV1Encoder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcoliApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcoliApplication.class, args);
+	}
+
+	@Bean
+	BytesEncoder<MutableSpan> otlpMutableSpanBytesEncoder() {
+		return OtlpProtoV1Encoder.create();
 	}
 }
